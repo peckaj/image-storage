587bdb6cba95183d78a28cb3692a7f97668a5e3c
Jiri Pecka <jiri.pecka@faceup.cz>
2018-11-05 16:05:39 +0100

    ff

--- src/Utils/Image.php
+++ src/Utils/Image.php
@@ -32,7 +32,7 @@ class Image extends \Nette\Utils\Image
 //			$this->image = imagecrop($this->image, $r);
 //		} else {
 			$newImage = static::fromBlank($r['width'], $r['height'], self::RGB(0, 0, 0, 127))->getImageResource();
-			imagecopy($newImage, $this->image, 0, 0, $r['x'], $r['y'], $r['width'], $r['height']);
+			imagecopy($newImage, $this->getImageResource(), 0, 0, $r['x'], $r['y'], $r['width'], $r['height']);
 			$this->image = $newImage;
 //		}
 		return $this;
